a97a10f9c8b9655fc2b937fa01daa3007b08a01d
Anshul Panda <anshul.panda@gmail.com>
2018-02-10 21:42:36 -0800

    First Upload
    Happy birthday!

---
+++ web/index.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+      <title>Amador Valley TSA Webmaster</title>
+        <!-- CSS -->
+        <!-- Boostrap -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+        <!-- Custom -->
+        <link rel="stylesheet" href="css/custom.css" type="text/css">
+        <script src="js/main.js"></script>
+    </head>
+    <body>
+        <ul class="custom-header">
+            <div class="header-elements">
+                <li class="header-title">
+                    <a href="information">Amador Valley</a>
+                </li>
+                <li class="header-element">
+                    <a href="#" class="header-element-text" id="home">Home</a>
+                </li>
+                <li class="header-element">
+                    <a href="#" class="header-element-text" id="about">About Us</a>
+                </li>
+                <li class="header-element">
+                    <a href="#" class="header-element-text" id="tsachapter">Our TSA Chapter</a>
+                </li>
+                 <li class="header-element">
+                    <a href="#" class="header-element-text" id="program">Engineering and Technology Program</a>
+                </li>
+                 <li class="header-element">
+                    <a href="#" class="header-element-text" id="ai">Artificial Intellegence</a>
+                </li>
+            </div>
+        </ul>
+        <br>
+        <center>
+            <div class="container">
+              <h2>Busac Noodle!</h2>
+              <div class="panel panel-default">
+                <div class="panel-body">We need to research so we can put information here.</div>
+              </div>
+            </div>
+        </center>
+    </body>
+</html>
