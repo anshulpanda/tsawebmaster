c97a6a8bdcf0d1d995c3900267b9a723453ab055
Anshul Panda <anshul.panda@gmail.com>
2018-02-11 20:42:12 -0800

    Add files via upload

---
+++ web/web/about.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+      <title>Amador Valley TSA Webmaster</title>
+        <!-- CSS -->
+        <!-- Boostrap -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+        <!-- Custom -->
+        <link rel="stylesheet" href="css/custom.css" type="text/css">
+        <script src="js/main.js"></script>
+    </head>
+    <body>
+        <?php include "header.php"; ?>
+        <br>
+        <center>
+            <div class="container">
+              <h2>Bloosac Noodle!</h2>
+              <div class="panel panel-default">
+                <div class="panel-body">We need to research so we can put information here.</div>
+              </div>
+            </div>
+        </center>
+        <script>
+          document.getElementById("about").className = "selected";
+        </script>
+    </body>
+</html>
---
+++ web/web/js/main.js
@@ -0,0 +1,4 @@
+// Detecting selecting for header
+var header_pages = {
+  "home": "index.php"
+}
